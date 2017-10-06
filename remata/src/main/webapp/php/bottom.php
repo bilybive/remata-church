5ef90ebcd465995fbbdbfba6f619e39438163fda
Administrator <Administrator@220.74.123.103>
2017-10-06 22:51:50 +0900

    title change

--- remata/src/main/webapp/php/bottom.php
+++ remata/src/main/webapp/php/bottom.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 	<meta charset="utf-8">
-	<title> 샘긷는교회 </title>
+	<title> 샘긷는교회 temp </title>
 	<!--[if lt IE 9]><script stc="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
 	<link href="/style.css" rel="stylesheet" type="text/css"><link href="/substyle.css" rel="stylesheet" type="text/css">
 	<script src="/js/jquery-1.11.2.js"></script><script src="/js/jquery.js"></script>
